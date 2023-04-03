a9bf6c7b7e798a1b9a9f8ef7d3996e3006acde5b
Erwin Brow Martinez <86860968+ErwinMartinezH@users.noreply.github.com>
2023-04-03 01:03:37 -0600

    Erwin
    vs. 1.0

---
+++ src/principal/Llama.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package principal;
+
+import interfaz.*;
+
+/**
+ *
+ * @author erwin
+ */
+
+ // Clase Llama que es main
+
+public class Llama {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Crear un objeto de la clase Ventana
+        Turing v = new Turing();
+        // Hacer visible la ventana
+        v.setVisible(true);
+    }
+}
